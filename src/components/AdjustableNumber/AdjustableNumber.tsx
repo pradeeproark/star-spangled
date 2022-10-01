51a9c8f44c9701cbb96197e17e12b0cd0b5a1b9f
Pradeep Roark <pradeep.roark@gmail.com>
2022-10-01 17:51:59 +0100

    feat: Initial component and build setup

---
+++ src/components/AdjustableNumber/AdjustableNumber.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export interface AdjustableNumberProps {
+    displayNumber : number;
+}
+
+const AdjustableNumber = (props : AdjustableNumberProps) => {
+    return <span>{props.displayNumber}</span>;
+};
+
+export default AdjustableNumber;
