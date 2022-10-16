af67139779e7f512e73d9bea70b0287636e9667a
Pradeep Roark <pradeep.roark@gmail.com>
2022-10-16 16:18:28 +0100

    chore: bump jest and config, update package name

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,8 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'jsdom',
+  preset: "ts-jest",
+  testEnvironment: "jsdom",
   transform: {
-    "\\.css\\.ts$": "@vanilla-extract/jest-transform"
-  }
+    "\\.css\\.ts$": "@vanilla-extract/jest-transform",
+  },
 };
