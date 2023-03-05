57eccc307f4ef1eb1541c4652cc5c0b5c1f05fa0
Pradeep Roark <pradeep.roark@gmail.com>
2023-03-05 17:00:09 +0000

    feat: update story

--- src/components/AdjustableNumber/AdjustableNumber.stories.tsx
+++ src/components/AdjustableNumber/AdjustableNumber.stories.tsx
@@ -1,29 +1,36 @@
-import {Story} from '@storybook/react';
-import React from 'react';
-import AdjustableNumber, {AdjustableNumberProps, DISPLAYMODE} from './AdjustableNumber';
+import { Story } from "@storybook/react";
+import React from "react";
+import AdjustableNumber, {
+  AdjustableNumberProps,
+  DISPLAYMODE,
+} from "./AdjustableNumber";
 
 export default {
-    title : 'AdjustableNumber',
-    component : AdjustableNumber,
-    argTypes : {
-        displayNumber: {
-            control: {
-                type: 'number'
-            }
-        },
-        displayMode: {
-            control: 'radio',
-            options: [
-                DISPLAYMODE.DISPLAY, DISPLAYMODE.EDIT
-            ],
-            defaultValue: DISPLAYMODE.DISPLAY
-        },
-        varName: {
-            control: 'text'
-        }
-    }
+  title: "AdjustableNumber",
+  component: AdjustableNumber,
+  argTypes: {
+    displayNumber: {
+      control: {
+        type: "number",
+      },
+    },
+    displayMode: {
+      control: "radio",
+      options: [DISPLAYMODE.DISPLAY, DISPLAYMODE.EDIT],
+      defaultValue: DISPLAYMODE.DISPLAY,
+    },
+    varName: {
+      control: "text",
+    },
+  },
 };
 
-const Template : Story < AdjustableNumberProps > = (args : JSX.IntrinsicAttributes & AdjustableNumberProps) => <AdjustableNumber {...args}/>
+const Template: Story<AdjustableNumberProps> = (
+  args: JSX.IntrinsicAttributes & AdjustableNumberProps
+) => (
+  <p>
+    Click on the number to adjust <AdjustableNumber {...args} />
+  </p>
+);
 
 export const DefaultAdjustableNumber = Template.bind({});
