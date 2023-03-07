064c70c153419de9d7ac950642c9d0afe7d3da26
nektos/act <nektos/act@users.noreply.github.com>
2023-03-07 10:08:57 +0000

    Deploying to main from @ pradeeproark/star-spangled@3ad4c854b3c225367312b9aa361af8777fcff065 🚀

--- src/interfaces/Common.ts
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-interface IVarContext {
-    vars: Map<string, number>;
-    updateVars: ((k: string, v: number) => void) | undefined;
-}
-
-export const VarContext = React.createContext<IVarContext>({ vars: new Map(), updateVars: undefined });
